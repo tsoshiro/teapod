07c2682aeec5bf34d1ed4c8842c77e16b5f626c9
tsoshiro <tsubasa.oshiro@gmail.com>
2019-01-05 03:24:05 +0900

    routing途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'play_entry/home'
+  root  'play_entry/home'
+  get   '/input', to: 'play_entry#home'
+  post  '/input', to: 'play_entry#get_entry'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
