0501ef80d827305f2cd839a2d6ca6b2f070066a9
tsoshiro <tsubasa.oshiro@gmail.com>
2019-01-06 08:19:52 +0900

    Contentモデルの作成

--- app/controllers/play_entry_controller.rb
+++ app/controllers/play_entry_controller.rb
@@ -7,17 +7,16 @@ class PlayEntryController < ApplicationController
   end
 
   def home
-    # test_url = "http://tnakamura.hatenablog.com/entry/2013/06/29/204726"
-    # test_url = "https://note.mu/knz2g/n/n43e38e89bd05"
-    # @test_url = "https://blog.craftz.dog/how-ive-attracted-the-first-500-paid-users-for-my-saas-that-costs-5-mo-4111ddad9f50"
-    # @test_url = "https://craftzdog.hateblo.jp/entry/i-spend-most-of-my-revenues-from-my-app-on-self-investment"
-    @test_url = "https://craftzdog.hateblo.jp/entry/healing-atopic-without-steroids"
+    arr_var = [
+      "http://tnakamura.hatenablog.com/entry/2013/06/29/204726",
+      "https://note.mu/knz2g/n/n43e38e89bd05",
+      "https://blog.craftz.dog/how-ive-attracted-the-first-500-paid-users-for-my-saas-that-costs-5-mo-4111ddad9f50",
+      "https://craftzdog.hateblo.jp/entry/i-spend-most-of-my-revenues-from-my-app-on-self-investment"
+    ]
 
-    open(@test_url) do |io|
-      html = io.read
-      @body, @title = ExtractContent.analyse(html)
-      puts @title
-      puts @body
+    @arr_cont = []
+    arr_var.each do |url|
+      @arr_cont << self.class.helpers.get_contents(url)
     end
   end
 end
--- app/helpers/play_entry_helper.rb
+++ app/helpers/play_entry_helper.rb
@@ -1,2 +1,15 @@
 module PlayEntryHelper
+  def get_contents(url = '')
+    content = Content.new
+
+    open(url) do |io|
+      html = io.read
+      body,title = ExtractContent.analyse(html)
+
+      content.body = body
+      content.title = title
+    end
+    content.url = url
+    content
+  end
 end
