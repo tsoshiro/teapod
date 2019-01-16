7d44ff4e788ce157ac56f37fc02de288e5e19a93
tsoshiro <tsubasa.oshiro@gmail.com>
2019-01-17 02:57:44 +0900

    テスト書き始め

--- test/controllers/play_entry_controller_test.rb
+++ test/controllers/play_entry_controller_test.rb
@@ -1,9 +1,31 @@
 require 'test_helper'
 
 class PlayEntryControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @content = Content.new
+  end
+
   test "should get home" do
-    get play_entry_home_url
+    get root_url
     assert_response :success
   end
 
+  # URLバリデーション
+  test "input url validation" do
+    url = 'http://example.com'
+
+    post input_path(@content), params: { content: { url: url } }
+
+    assert flash.empty?
+
+    url = 'https://www.example.com'
+    post input_path(@content), params: { content: { url: url } }
+
+    assert flash.empty?
+
+    url = 'http.example.com'
+    post input_path(@content), params: { content: { url: url } }
+
+    assert_not flash.empty?
+  end
 end
